0ceee7a42c4e7b8e8df71a631d7e96a891a11c32
raihanhosen011 <raihanhosen011@gmail.com>
2022-12-14 19:52:13 +0600

    Add Button component

---
+++ src/shared/config/constants.ts
@@ -0,0 +1,5 @@
+import classNames from "classnames";
+
+
+// whenever wee need to change default className value we will use this
+export const cx = classNames;
---
+++ src/shared/config/types.ts
@@ -0,0 +1,19 @@
+import { ButtonHTMLAttributes, ReactNode } from "react";
+
+
+// button props
+export interface ButtonProps {
+    className?: string
+    translate?: string
+    sizeClass?: string
+    fontSize?: string
+    //
+    loading?: boolean
+    disabled?: boolean
+    secondary?: boolean
+    type?: ButtonHTMLAttributes<HTMLButtonElement>["type"]
+    href?: string
+    targetBlank?: boolean
+    onClick?: () => void
+    children?: ReactNode
+};
