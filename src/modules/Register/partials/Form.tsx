e2beec7eef67acd98c5140c01d5d49ab0402821d
raihanhosen011 <raihanhosen011@gmail.com>
2022-12-17 15:14:40 +0600

    oparation for registration component

--- src/modules/Register/partials/Form.tsx
+++ src/modules/Register/partials/Form.tsx
@@ -1,29 +1,49 @@
-import React from 'react'
+"use client"
+
+import React, { useState } from 'react'
 import Link from 'next/link'
+//
+import { FormSubmit, InputChange } from '@config/types'
+import { regInitial } from '../config/constants'
+import { IRegistration } from '../config/types'
 //
 import Button from '@components/Button'
 
 
 const Form = () => {
+  // state
+  const [regData, setRegData] = useState<IRegistration>(regInitial)
+
+  // handle input value 
+  const handleChange = (e: InputChange) => {
+    const { value, name } = e.target
+    setRegData({ ...regData, [name] : value })
+  }
+  
+  // handle suvmit form
+  const handleSubmit = (e: FormSubmit) => {
+    e.preventDefault()
+  }
+
   return (
     <div className='__login' >
-      <form>
-         
-        <input placeholder='Enter your name' type='text' required />
-        <input placeholder='Enter your email' type='email' required />
-        <input placeholder='Enter password' type='text' required />
+      <form onSubmit={handleSubmit} >
+
+        <input onChange={handleChange} name="name" placeholder='Enter your name' type='text' required />
+        <input onChange={handleChange} name="account" placeholder='Enter your email' type='email' required />
+        <input onChange={handleChange} name="password" placeholder='Enter password' type='text' required />
         
         <div className='flex items-center justify-between mb-5 text-gray-700 mx-3' >
           <h2> Verify mail via </h2>
 
           <div className='flex gap-4' >
             <div>
-               <input type='radio' value='by_link' name='type' defaultChecked required />
+               <input onChange={handleChange} type='radio' value='verify_link' name='type' required />
                <label> Link </label>  
             </div>
 
             <div>
-               <input type='radio' value='by_code' name='type' required />
+               <input onChange={handleChange} type='radio' value='verify_code' name='type' defaultChecked required />
                <label> Code </label>  
             </div>
           </div>
