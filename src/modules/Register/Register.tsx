083233f74a026cdca7fd5761e7994224346e23f9
raihanhosen011 <raihanhosen011@gmail.com>
2023-01-12 11:55:42 +0600

    coded

--- src/modules/Register/Register.tsx
+++ src/modules/Register/Register.tsx
@@ -2,15 +2,13 @@
 import React from 'react'
 //
 import Form from './partials/Form'
-import ByLink from './partials/ByLink'
-
 
 const Register = () => {
   return (
     <div className='flex flex-col items-center justify-center min-h-screen' >
            
         <div className='mt-[58px]' >
-          <ByLink />
+          <Form />
         </div>
  
     </div>
