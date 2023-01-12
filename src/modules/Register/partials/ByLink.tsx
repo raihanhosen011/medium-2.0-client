6f135d542dd56f3fb5a08a9aa42f9aa5cbf07dce
raihanhosen011 <raihanhosen011@gmail.com>
2023-01-12 11:55:50 +0600

    coded

--- src/modules/Register/partials/ByLink.tsx
+++ src/modules/Register/partials/ByLink.tsx
@@ -6,9 +6,9 @@ import Button from '@components/Button'
 
 const ByLink = () => {
   return (
-    <div>
+    <div className='flex items-center justify-center flex-col' >
 
-        <div className='w-[60px] relative overflow-hidden mx-auto' >
+        <div className='min-w-[60px] relative overflow-hidden mx-auto' >
            <Image
               src="/icons/img/check.png" 
               blurDataURL="/icons/img/check.png"
@@ -21,7 +21,9 @@ const ByLink = () => {
            />
         </div>
 
-        <p className='font-medium text-[16px] md:text-[19px] max-w-[570px] text-slate-600 my-6' > Your account has been successfully created. Please check your <b>email</b> and verify to active your account </p>
+        <p className='font-medium text-center text-[16px] md:text-[19px] min-w-[570px] text-slate-600 my-6' > Your account has been successfully created. Please check your <b>email</b> and verify to active your account </p>
+
+        <Button href='/' > OK </Button>
 
     </div>
   )
